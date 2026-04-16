32daad6b6985c6a415331ef288188d0448c9fb8f
Bautista <Bautistasoto918@gmail.com>
2026-04-16 02:48:03 -0300

    Refactor project structure: rename package, update main entry point, remove TypeScript config, add JavaScript config, and implement main server file with Express and CORS middleware.

---
+++ main.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");       
+
+const app = express();
+const PORT = 3000;
+
+// --- Middlewares ---
+app.use(cors()); 
+app.use(express.json()); 
+
+// Endpoints
+
+app.get("/", (req, res) => {
+    res.send("Servidor de Tickets Funcionando");
+});
+
+
+app.post("/comprar-ticket", (req, res) => {
+    const { nombreComprador, evento, cantidad } = req.body;
+
+    if (!nombreComprador || !evento || !cantidad) {
+        return res.status(400).json({
+            error: "Error: Faltan datos para procesar la compra del ticket.",
+        });
+    }
+
+    console.log(`[LOG] Nueva compra de ${nombreComprador} para ${evento}`);
+
+    res.status(201).json({
+        mensaje: "¡Ticket comprado exitosamente!",
+        resumenCompra: {
+            nombreComprador,
+            evento,
+            cantidad,
+        },
+    });
+});
+
+// --- Iniciar Servidor ---
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
